db7d25b50a2c794b7aca914053a320c824cfc017
proprdev <propruhh@gmail.com>
2021-10-26 21:03:16 -0500

    remove unneccesary thingys

--- include/Networking.hpp
+++ include/Networking.hpp
@@ -1,12 +1,9 @@
 #ifndef NETWORKING_HPP
-
 #define NETWORKING_HPP
 
 #include <curl/curl.h>
 #include <string>
 #include "../include/misc.hpp"
-
-#define SEND_MESSAGE_URL "https://discord.com/api/v9/channels/899322176942010368/messages"
 
 void sendMessage(std::string token, std::string guild_id, std::string channel_id, std::string content);
 std::string getMessages(std::string token, std::string guild_id, std::string channel_id, std::string limit = "50");
